ad331e40a48dfeaa8529b051779c4684ae6e290c
= <kevingruber01@protonmail.com>
2025-12-23 11:38:18 +0100

    switch to postgres image

--- chirpstack/configuration/postgresql/initdb/001-chirpstack_extensions.sh
+++ chirpstack/configuration/postgresql/initdb/001-chirpstack_extensions.sh
@@ -2,6 +2,7 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname="$POSTGRES_DB" <<-EOSQL
-    create extension pg_trgm;
-    create extension hstore;
+    -- Create extensions if they don't exist (idempotent)
+    CREATE EXTENSION IF NOT EXISTS pg_trgm;
+    CREATE EXTENSION IF NOT EXISTS hstore;
 EOSQL
